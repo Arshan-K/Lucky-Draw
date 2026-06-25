ce5b5add6524ec189e1ab3f89ac29a0e82971d32
Arshan <arshankhan9326863763@gmail.com>
2026-06-25 22:37:07 +0530

    Bug Fixes

--- src/utils/slotLogic.js
+++ src/utils/slotLogic.js
@@ -7,21 +7,25 @@ export const generateSpinGrid = () => {
 };
 
 export const calculateWin = (grid, bet) => {
-  let win = 0;
+  let totalWin = 0;
 
   grid.forEach((row) => {
-    const counts = {};
+    let currentStreak = 1;
+    let maxStreak = 1;
 
-    row.forEach((symbol) => {
-      counts[symbol] = (counts[symbol] || 0) + 1;
-    });
-
-    Object.values(counts).forEach((count) => {
-      if (count >= 3) {
-        win += count * bet;
+    for (let i = 1; i < row.length; i++) {
+      if (row[i] === row[i - 1]) {
+        currentStreak++;
+        maxStreak = Math.max(maxStreak, currentStreak);
+      } else {
+        currentStreak = 1;
       }
-    });
+    }
+
+    if (maxStreak >= 3) {
+      totalWin += maxStreak * bet;
+    }
   });
 
-  return win;
+  return totalWin;
 };
